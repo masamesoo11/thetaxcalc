49db8a6f977e9b1787804c130b5fe31043d01c2f
Z User <z@container>
2026-05-31 05:06:16 +0000

    cleanup: remove temporary files

--- keep-dev-alive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-  echo "[$(date)] Starting dev server..." >> /home/z/my-project/dev-alive.log
-  npx next dev -p 3000 >> /home/z/my-project/dev.log 2>&1
-  EXIT_CODE=$?
-  echo "[$(date)] Dev server exited with code $EXIT_CODE" >> /home/z/my-project/dev-alive.log
-  sleep 3
-done
